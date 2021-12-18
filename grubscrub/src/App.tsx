7a7f365e153592e474061a4a7b1407e98b0afa79
cbrandt6 <cbrandt6@ycp.edu>
2021-12-18 15:37:39 -0500

    Basic setup

--- grubscrub/src/App.tsx
+++ grubscrub/src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  return <div className='App'> 
-          <Header />
-        </div>
+  return <div className='App'>
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;400;700&display=swap');
+    </style>
+    <Header />
+  </div>
 }
 
 // const TestComp = () => {
--- grubscrub/src/components/Header.tsx
+++ grubscrub/src/components/Header.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import '../styles/header.css';
+import { Button } from 'react-bootstrap';
 
 
 
 const Header = () => {
-    return <div> <h1 className="Title" > GrubScrub </h1></div>
+    return <div> 
+        <h1 className="Title" > Welcome to Grub Scrub! </h1>
+        <Button variant="outline-primary">Search</Button>{' '}
+    </div>
 }
 
 export default Header;
