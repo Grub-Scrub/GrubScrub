4d8956cbca5a7efd436f4691cdaef9cbfc2e7e06
cbrandt6 <cbrandt6@ycp.edu>
2021-12-18 15:09:55 -0500

    Helping out Carl

--- grubscrub/src/App.tsx
+++ grubscrub/src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-//import './styles/header.css';
-import * as Header from './components/Header';
+import Header from './components/Header';
 
 function App() {
-  return (
-    //<div className="App"> <Header /></div>
-  );
+  return <div className='App'> 
+          <Header />
+        </div>
 }
 
-const TestComp = () => {
-  return <div><h1>Test...............................</h1></div>
-}
+// const TestComp = () => {
+//   return <div><h1>Test...............................</h1></div>
+// }
 
 
 
--- grubscrub/src/components/Header.tsx
+++ grubscrub/src/components/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import './styles/header.css';
+import '../styles/header.css';
 
-const Header = (Elem: React.ComponentClass<any>) => {
-    return <div> <h1 className="Title" > GrubScrub <Elem /></h1></div>
+
+
+const Header = () => {
+    return <div> <h1 className="Title" > GrubScrub </h1></div>
 }
 
 export default Header;
